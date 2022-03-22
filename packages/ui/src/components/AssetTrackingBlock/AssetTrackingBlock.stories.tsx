c1762e8a51c956ebdd640cbe824553e0b3e4a61a
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-03-23 00:00:56 +0100

    Add figma viewer plugin to storybook

--- packages/ui/src/components/AssetTrackingBlock/AssetTrackingBlock.stories.tsx
+++ packages/ui/src/components/AssetTrackingBlock/AssetTrackingBlock.stories.tsx
@@ -3,6 +3,7 @@ import { ComponentStory, type ComponentMeta } from "@storybook/react";
 import Component from "./AssetTrackingBlock";
 import { Bitcoin } from "../../icons";
 import Google from "../../icons/Google";
+import { withDesign } from "storybook-addon-designs";
 
 export default {
   title: "PWA/Asset Tracking Block",
@@ -15,6 +16,7 @@ export default {
       },
     },
   },
+  decorators: [withDesign],
 } as ComponentMeta<typeof Component>;
 
 const Template: ComponentStory<typeof Component> = (args) => (
@@ -24,6 +26,12 @@ const Template: ComponentStory<typeof Component> = (args) => (
 //👇 Each story then reuses that template
 export const Crypto = Template.bind({});
 Crypto.args = { amount: "87,2K", gain: "+47%", Icon: <Bitcoin /> };
+Crypto.parameters = {
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/ELu4gwRGS5i2f6XSggCC2P/Virtual-Friends-Website?node-id=167%3A146",
+  },
+};
 
 export const Stock = Template.bind({});
 Stock.args = { amount: "23,3", gain: "+22%", Icon: <Google /> };

c65de50938506214e8f22500ebf6dc9abd483234
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-03-23 13:17:32 +0100

    Make AssetsTabBar interactive

--- packages/ui/src/components/AssetsTabBar/AssetsTabBar.stories.tsx
+++ packages/ui/src/components/AssetsTabBar/AssetsTabBar.stories.tsx
@@ -1,10 +1,33 @@
-import React from "react";
+import React, { useState } from "react";
 import { type ComponentMeta } from "@storybook/react";
 import Component from "./AssetsTabBar";
+import { Bitcoin, Google, Dollar } from "../../icons";
 
 export default {
   title: "PWA/Assets Tab Bar",
   component: Component,
+  parameters: {
+    design: {
+      type: "figma",
+      url: "https://www.figma.com/file/ELu4gwRGS5i2f6XSggCC2P/Virtual-Friends-Website?node-id=212%3A144",
+    },
+  },
 } as ComponentMeta<typeof Component>;
 
-export const AssetsTabBar = () => <Component />;
+const tabs = [
+  { title: "Crypto", icon: <Bitcoin />, value: "crypto" },
+  { title: "Stocks", icon: <Google />, value: "stocks" },
+  { title: "Fiat", icon: <Dollar />, value: "fiat" },
+];
+
+export const AssetsTabBar = () => {
+  const [value, setValue] = useState("crypto");
+
+  return (
+    <Component
+      setTab={(newValue) => setValue(newValue)}
+      tabs={tabs}
+      currentTab={value}
+    />
+  );
+};
--- packages/ui/src/components/AssetsTabBar/AssetsTabBar.tsx
+++ packages/ui/src/components/AssetsTabBar/AssetsTabBar.tsx
@@ -1,27 +1,43 @@
 import React from "react";
-import { Bitcoin, Google, Dollar } from "../../icons";
 const boxShadow =
   "0px 4px 5px rgba(0, 0, 0, 0.3), 0px 9px 10px rgba(0, 0, 0, 0.2), 0px 14px 32px rgba(0, 0, 0, 0.1), 0px 32px 44px rgba(0, 0, 0, 0.25)";
 
-const AssetsTabBar: React.FC = () => {
+export interface AssetsTabBarProps {
+  tabs: { title: string; icon: JSX.Element; value: string }[];
+  setTab: (value: string) => void;
+  currentTab: string;
+}
+
+const AssetsTabBar: React.FC<AssetsTabBarProps> = ({
+  tabs,
+  setTab,
+  currentTab,
+}) => {
   return (
     <div>
       <div
         className="inline-flex overflow-hidden rounded-lg bg-[#201D2A] text-white"
         style={{ boxShadow }}
       >
-        <button className="flex w-36 items-center justify-center gap-1 border-r border-r-[#2E2A3C] bg-gradient-to-b from-[#A93BFF] to-[#8834FF] py-2 font-bold">
-          <Bitcoin />
-          <span>Crypto</span>
-        </button>
-        <button className="flex w-36 items-center justify-center gap-1 border-r border-r-[#2E2A3C] py-2 font-bold">
-          <Google />
-          <span>Stocks</span>
-        </button>
-        <button className="flex w-36 items-center justify-center gap-1 py-2 font-bold">
-          <Dollar />
-          <span>Fiat</span>
-        </button>
+        {tabs.map((el, index) => {
+          return (
+            <button
+              className={`flex w-36 items-center justify-center gap-1
+              ${
+                currentTab == el.value
+                  ? "bg-gradient-to-b from-[#A93BFF] to-[#8834FF]"
+                  : ""
+              }
+
+              ${index < tabs.length - 1 ? "border-r border-r-[#2E2A3C]" : ""}
+               py-2 font-bold`}
+              onClick={() => setTab(el.value)}
+            >
+              {el.icon}
+              <span className=" font-body text-lg">{el.title}</span>
+            </button>
+          );
+        })}
       </div>
     </div>
   );

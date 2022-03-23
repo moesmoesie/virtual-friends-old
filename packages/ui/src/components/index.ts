e6d9862c94d073537bd0e57d46832fa58a6553ae
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-03-23 15:01:01 +0100

    Refactor

--- packages/ui/src/components/index.ts
+++ packages/ui/src/components/index.ts
@@ -1,5 +1,3 @@
 export { default as Button } from "./Button";
-export { default as RadialGradient } from "./RadialGradient";
-export { default as GlassCard } from "./Card/GlassCard";
 export { default as AssetTrackingBlock } from "./AssetTrackingBlock";
 export { default as AssetsTabBar } from "./AssetsTabBar";

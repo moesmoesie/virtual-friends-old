9885e5d4aabdcb6199cf31dc22f2aa8a32734831
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-03-26 15:50:43 +0100

    Add a11y addon storybook

--- apps/storybook/.storybook/main.js
+++ apps/storybook/.storybook/main.js
@@ -1,11 +1,12 @@
 module.exports = {
   stories: ["../../../packages/ui/**/*.stories.@(js|jsx|ts|tsx)"],
   addons: [
-    "storybook-zeplin/register",
     "@storybook/addon-links",
     "@storybook/addon-essentials",
     "@storybook/addon-interactions",
     "storybook-addon-designs",
+    "storybook-zeplin/register",
+    "@storybook/addon-a11y", //👈 The a11y addon goes here
   ],
   framework: "@storybook/react",
   core: {

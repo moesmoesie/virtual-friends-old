3338686326db15167d3ee8228cb21195d235da9e
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-03-31 17:19:43 +0200

    Use tailwind to add shadows and gradients to buttons

--- packages/ui/components/Button/AccentButton/AccentButton.tsx
+++ packages/ui/components/Button/AccentButton/AccentButton.tsx
@@ -8,7 +8,7 @@ export interface AccentButtonProps {
 const AccentButton: React.FC<AccentButtonProps> = (props) => {
   return (
     <button
-      className="body-3 bg-purple-gradient box-shadow-1 rounded-lg py-3 px-8 text-white"
+      className="body-3 bg-purple-gradient rounded-lg py-3 px-8 text-white shadow-lg shadow-[#7947F7]/50"
       onClick={props.onClick}
     >
       {props.label}
--- packages/ui/components/Button/DefaultButton/DefaultButton.tsx
+++ packages/ui/components/Button/DefaultButton/DefaultButton.tsx
@@ -5,16 +5,10 @@ export interface DefaultButtonProps {
   onClick: () => void;
 }
 
-const style = {
-  boxShadow:
-    "0 14px 32px 0 rgba(238, 238, 238, 0.2), 0 9px 10px 0 rgba(196, 196, 196, 0.3), 0 4px 5px 0 rgba(64, 201, 162, 0.28)",
-};
-
 const DefaultButton: React.FC<DefaultButtonProps> = (props) => {
   return (
     <button
-      style={style}
-      className="body-3 rounded-lg bg-teal-500 py-3 px-8 text-white"
+      className="body-3 rounded-lg bg-teal-500 py-3 px-8 text-white shadow-lg shadow-teal-500/50"
       onClick={props.onClick}
     >
       {props.label}

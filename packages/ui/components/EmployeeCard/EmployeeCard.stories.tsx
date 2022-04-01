95573977e83f45ee3cbe82b84dd3c8b3c8faf2e3
Mustafa Darwesh <mndarwesh@protonmail.com>
2022-04-01 16:18:39 +0200

    Create company card

--- packages/ui/components/EmployeeCard/EmployeeCard.stories.tsx
+++ packages/ui/components/EmployeeCard/EmployeeCard.stories.tsx
@@ -13,10 +13,18 @@ const Template: ComponentStory<typeof Component> = (args) => (
 
 export const EmployeeCard = Template.bind({});
 EmployeeCard.args = {
-  label: "Hello World!",
+  name: "John Doe",
+  company: "A virtual Friends",
+  phone: "+31634XXX366",
+  src: "https://www.dropbox.com/s/w7st1ghpq8mmcmj/person.png?raw=1",
+  github: "https://github.com/moesmoesie",
+  mail: "",
+  linkedIn: "linked",
 };
 
 EmployeeCard.parameters = {
-  zeplinLink:
-    "zpl://components?pid=62339c5b4312c8b1de07b037&coid=6240e0588ccd8211aab08e4a",
+  design: {
+    type: "figma",
+    url: "https://www.figma.com/file/ELu4gwRGS5i2f6XSggCC2P/Virtual-Friends-Website?node-id=333%3A568",
+  },
 };
